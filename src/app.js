d94fdb9ed2e094b85eb0f943237ef4d1952bffb4
mr-aman-git <amanazr2003@gmail.com>
2025-04-13 20:43:43 +0530

    fix cors origin

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 
 
